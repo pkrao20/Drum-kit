a39d0277f15699683f791b0dd393f996bd410f6e
Prashant rao <95528457+pkrao20@users.noreply.github.com>
2022-08-23 14:22:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,81 @@
-// document.querySelectorAll("button")[0].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[1].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[2].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[3].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[4].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[5].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// document.querySelectorAll("button")[7].addEventListener("click",function(){
-//     alert("mat click kr bsdk");
-// });
-// all above code written using a loop idiot;
-for(var d=0;d<7;d++){
-    document.querySelectorAll("button")[d].addEventListener("click",function(){
-        var buttonhtml=this.innerHTML;
-        switch (buttonhtml) {
-            case "w":
-                var tom1=new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-            case "a": 
-                var tom2=new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            case "s": 
-                var tom3=new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;
-            case "d": 
-                var tom4=new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;
-            case "j": 
-                var snr=new Audio('sounds/snare.mp3');
-                snr.play();
-                break;
-            case "k": 
-                var crsh=new Audio('sounds/crash.mp3');
-                crsh.play();
-                break;
-            case "l": 
-                var kik=new Audio('sounds/kick-bass.mp3');
-                kik.play();
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-            break;
-            
-        }
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-    });
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
 }
-// var audio=new Audio('sounds/tom-1.mp3');
-// audio.play();
 
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
